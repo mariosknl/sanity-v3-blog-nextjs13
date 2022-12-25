cb2edd034979707ac94e59388aa7657d60d7680b
mariosknl <mariosknl@outlook.com>
2022-12-25 09:19:22 +0200

    Building the Blog Article Content Page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,24 +1,37 @@
+import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./schemas";
-import { myTheme } from "./theme";
-import StudioNavbar from "./components/StudioNavbar";
 import Logo from "./components/Logo";
+import StudioNavbar from "./components/StudioNavbar";
+
+import { schemaTypes } from "./schemas";
+import { getDefaultDocumentNode } from "./structure";
+import { myTheme } from "./theme";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-	basePath: "/studio",
-	name: "PAPAFAM_Content_Studio",
-	title: "PAPAFAM Content Studio",
+	basePath: "/studio", // <-- important that `basePath` matches the route you're mounting your studio from, it applies to both `/pages` and `/app`
+
 	projectId,
 	dataset,
-	plugins: [deskTool(), visionTool()],
+
+	plugins: [
+		deskTool({
+			defaultDocumentNode: getDefaultDocumentNode,
+		}),
+		visionTool(),
+	],
+
 	schema: {
 		types: schemaTypes,
 	},
+	icon: Logo,
+	logo: Logo,
+	subtitle: "Login to manage the Blog",
+	title: "PAPAFAM Content Studio",
+	name: "PAPAFAM_Content_Studio",
 	studio: {
 		components: {
 			logo: Logo,
